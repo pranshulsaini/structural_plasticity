3ea9e61e7bc7257f5468ef9843f0e11bb6d02e95
pranshulsaini <pranshulsaini17@gmail.com>
2017-06-19 12:38:16 +0200

    finished writing code in brian, although there is some error. I am not able to complile.I have not figured out how to keep the zero synapses at zero throughout the simulation. Also, I do not know how Robin changed ge and gi with spikes

--- brian_stdp.py
+++ brian_stdp.py
@@ -1,6 +1,5 @@
 #no structural plasticity in this code. Only stdp synapses. The output should stabilise to input gaussian pattern after some time
 
-import nest
 import math
 import cmath
 import numpy
@@ -69,14 +68,14 @@ class stdp_class:
     def __init__(self):
 
         # simulated time (ms)
-        self.t_sim = 1000*250.0    # This training case. 1000 examples, each for 250ms. It would 200000 in test case
+        self.t_sim = 1000*250.0 *ms    # This training case. 1000 examples, each for 250ms. It would 200000 in test case
         # simulation step (ms).
-        self.dt = 0.5
+        self.dt = 0.5 * ms
         #record time
-        self.record_interval = 1000.
+        self.record_interval = 25. *ms  # let's take 10 recordings per example
 
         #input change
-        self.input_change_interval = 250.0
+        self.input_change_interval = 250.0 * ms
 
         #input population
         self.number_input_neurons = 1600
@@ -89,7 +88,7 @@ class stdp_class:
         self.xdata = np.linspace(0, 1599, 1600) / 1600
         self.sigma_input = 1./6.
         self.mu_input = 0.5
-        self.bg_rate = 20. * wrap_gaussian(self.xdata, self.mu_input, self.sigma_input)
+        self.bg_rate = 20. * wrap_gaussian(self.xdata, self.mu_input, self.sigma_input) *Hz
 
         # will contain firing rate of excitatory neurons with time
         self.spikes = np.zeros(1600)
@@ -99,12 +98,12 @@ class stdp_class:
         self.y_fit = np.zeros(1600)
 
         # connections
-        self.wt_inp_e = np.zeros((self.number_excitatory_neurons, self.number_input_neurons))
-        self.wt_inp_i = np.zeros((self.number_inhibitory_neurons, self.number_input_neurons))
-        self.wt_e_e = np.zeros((self.number_excitatory_neurons, self.number_excitatory_neurons))
-        self.wt_e_i = np.zeros((self.number_inhibitory_neurons, self.number_excitatory_neurons))
-        self.wt_i_e = np.zeros((self.number_excitatory_neurons, self.number_inhibitory_neurons))
-        self.wt_i_i = np.zeros((self.number_inhibitory_neurons, self.number_inhibitory_neurons))
+        self.wt_inp_e = None
+        self.wt_inp_i = None
+        self.wt_e_e = None
+        self.wt_e_i = None
+        self.wt_i_e = None
+        self.wt_i_i = None
 
         #will contain the noise
         self.noise = []
@@ -144,16 +143,16 @@ class stdp_class:
         self.exp_pre_ee = 0.2
         self.exp_post_ee = self.exp_pre_ee
 
-        self.delay_inp_e = (10 * ms - 0 * ms) * np.random.random() + 0 * ms
-        self.delay_inp_i = (5 * ms - 0 * ms) * np.random.random() + 0 * ms
-        self.delay_e_e = (5 * ms - 0 * ms) * np.random.random() + 0 * ms
-        self.delay_e_i = (2 * ms - 0 * ms) * np.random.random() + 0 * ms
-        self.delay_i_e = ((1 * ms - 0 * ms) * np.random.random() + 0 * ms
-        self.delay_i_i = (5 * ms - 0 * ms) * np.random.random() + 0 * ms
+        self.delay_inp_e = None
+        self.delay_inp_i = None
+        self.delay_e_e = None
+        self.delay_e_i = None
+        self.delay_i_e = None
+        self.delay_i_i = None
 
         self.neuron_eqs_e = '''
                 dv/dt = ((self.v_rest_e-v) + (I_synE+I_synI) / nS) / (20*ms)  : volt
-                I_synE = ge * nS *         -v                           : amp
+                I_synE = ge * nS * -v                                   : amp
                 I_synI = gi * nS * (-85.*mV-v)                          : amp
                 dge/dt = -ge/(5.0*ms)                                   : 1
                 dgi/dt = -gi/(10.0*ms)                                  : 1
@@ -168,124 +167,146 @@ class stdp_class:
                 '''
 
         self.eqs_stdp_ee = '''
-                    w : 1  
-                    postbefore                        : 1.0
-                    dpre/dt   =   -pre/(self.tc_pre_ee)       : 1.0 (event-driven)
-                    dpost/dt = -post/(self.tc_post_ee)     : 1.0 (event-driven)
-                    w = clip(w+post, 0, self.wmax_ee)
-                    '''
+                w : 1  
+                postbefore                        : 1.0
+                dpre/dt   =   -pre/(self.tc_pre_ee)       : 1.0 (event-driven)
+                dpost/dt = -post/(self.tc_post_ee)     : 1.0 (event-driven)
+                '''
         self.eqs_stdp_ie = '''
-                    w : 1
-                    dpre/dt   =  -pre/(self.tc_pre_ie)        : 1.0 (event-driven)
-                    dpost/dt  = -post/(self.tc_post_ie)       : 1.0 (event-driven)
-                    w = clip(w+post, 0, self.wmax_ie)
-                    '''
-
+                w : 1
+                dpre/dt   =  -pre/(self.tc_pre_ie)        : 1.0 (event-driven)
+                dpost/dt  = -post/(self.tc_post_ie)       : 1.0 (event-driven)
+                '''
+        # added the conductance change and weight clipping below
         self.eqs_STDP_pre_ee = '''
-                        pre = 1.
-                        w -= self.nu_pre_ee * post * w**self.exp_pre_ee
-                        '''
-        self.eqs_STDP_post_ee = '''
-                    postbefore = post
-                    w += self.nu_post_ee * pre * postbefore * (wmax_ee - w)**exp_post_ee
-                    post = 1.
-                    '''
+                ge = ge + w
+                pre = 1.
+                w -= self.nu_pre_ee * post * w**self.exp_pre_ee
+                w = clip(w, 0, self.wmax_ee)
+                '''
 
+        self.eqs_STDP_post_ee = '''
+                postbefore = post
+                w += self.nu_post_ee * pre * postbefore * (wmax_ee - w)**exp_post_ee
+                post = 1.
+                w = clip(w, 0, self.wmax_ee)
+                '''
+        #The equations below were not mentioned in the paper
         self.eqs_STDP_pre_ie ='''
-                    pre += 1.
-                    w += self.nu_ie * (post-self.alpha_ie)
-                    '''
+                gi =  gi + w
+                pre += 1.
+                w += self.nu_ie * (post-self.alpha_ie)
+                w = clip(w, 0, self.wmax_ie)
+                '''
 
         self.eqs_STDP_post_ie ='''
-                    post += 1.
-                    w += self.nu_ie * pre
-                    '''
+                post += 1.
+                w += self.nu_ie * pre
+                w = clip(w, 0, self.wmax_ie)
+                '''
 
 
     def prepare_simulation(self):
-        nest.ResetKernel()
-        nest.set_verbosity('M_ERROR')
-        nest.SetKernelStatus(
-            {
-                'resolution': self.dt
-            }
-        )
+        defaultclock.dt = self.dt
+
 
 
     def create_nodes(self):
         self.nodes_inp = PoissonGroup(self.number_input_neurons, self.bg_rate)
-        self.nodes_e = NeuronGroup(self.number_excitatory_neurons, self.neuron_eqs_e, threshold= self.v_thresh_e, refractory= self.refrac_e, reset= self.v_reset_e, compile = True, freeze = True)
-        self.nodes_i = NeuronGroup(self.number_inhibitory_neurons, self.neuron_eqs_i, threshold= self.v_thresh_i, refractory= self.refrac_i, reset= self.v_reset_i, compile= True, freeze= True)
+        self.nodes_e = NeuronGroup(self.number_excitatory_neurons, self.neuron_eqs_e, threshold= 'v>self.v_thresh_e', refractory= self.refrac_e, reset= 'v = self.v_reset_e')
+        self.nodes_i = NeuronGroup(self.number_inhibitory_neurons, self.neuron_eqs_i, threshold= 'v>self.v_thresh_i', refractory= self.refrac_i, reset= 'v = self.v_reset_i')
 
     def connections(self):
         # we have to chose only 10% random connections in the beginning. I have created weight matrices for that
 
         # input population to excitatory neurons.
-
-
-
+        S_inp_e = Synapses(self.nodes_inp, self.nodes_e, model=self.eqs_stdp_ee, on_pre=self.eqs_STDP_pre_ee, on_post = self.eqs_STDP_post_ee)
+        S_inp_e.connect()  # all to all connections except to itself
         for j in range(0,1600):
             pos_inp_e =  np.random.randint(0,1600,160)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_inp_e = (10 * ms - 0 * ms) * np.random.random(160) + 0 * ms
+            self.wt_inp_e = np.random.rand(160)        # unformly distributed random numbers between 0 and 1
+            k = 0
             for i in pos_inp_e:
-                self.wt_inp_e[i][j] = np.random.random()   # weights have to be between 0 and 1
-        #'weight': self.wt_inp_e , 'delay': 1.0, 'tau_plus': 15, 'Wmax':1.0})
-        S = Synapses(self.nodes_inp, self.nodes_inp, model = self.eqs_stdp_ee, on_pre = self.eqs_STDP_pre_ee )
-        S.connect()  # all to all connections except to itself
-        S.delay = self.delay_inp_e  #It needs to be input as an array, not a matrix
-        S.w = self.wt_inp_e         # It needs to be input as an array, not a matrix
+                S_inp_e.w[i][j] = self.wt_inp_e[k]   # weights have to be between 0 and 1
+                S_inp_e.delay[i][j] =  self.delay_inp_e[k]
+                k = k+1
 
         #input population to inhibitory neurons.
-
-        for j in range(0, 1600):
-            pos_inp_i = np.random.randint(0, 400,40)  # assigns random 40 positions between [0 and 400). Acts as a column of connection matrix
+        S_inp_i = Synapses(self.nodes_inp, self.nodes_i, model=self.eqs_stdp_ee, on_pre=self.eqs_STDP_pre_ee,  on_post = self.eqs_STDP_post_ee)
+        S_inp_i.connect()  # all to all connections except to itself
+        for j in range(0, 400):
+            pos_inp_i = np.random.randint(0, 1600, 160)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_inp_i = (5 * ms - 0 * ms) * np.random.random(160) + 0 * ms
+            self.wt_inp_i = (1./5)*np.random.rand(160)  # unformly distributed random numbers between 0 and 0.2
+            k = 0
             for i in pos_inp_i:
-                self.wt_inp_i[i][j] = (1./5)*np.random.random()  # weights have to be between 0 and 0.2
-        nest.Connect(self.nodes_inp, self.nodes_i,conn_spec = {'rule': 'all_to_all'}, syn_spec = {'model': 'stdp_synapse', 'weight': self.wt_inp_i , 'delay': 1.0, 'tau_plus': 15, 'Wmax':0.2})
+                S_inp_i.w[i][j] = self.wt_inp_i[k]  # weights have to be between 0 and 1
+                S_inp_i.delay[i][j] = self.delay_inp_i[k]
+                k = k + 1
 
 
         #recurrent connections from excitatory neurons on itself.
-
+        S_e_e = Synapses(self.nodes_e, self.nodes_e, model=self.eqs_stdp_ee, on_pre=self.eqs_STDP_pre_ee,  on_post = self.eqs_STDP_post_ee)
+        S_e_e.connect('i !=j')  # all to all connections except to itself, Still I can go upon [1599,1599] but just can't change the diagonal elements
         for j in range(0, 1600):
-            pos_e_e = np.random.randint(0, 1600,160)
+            pos_e_e = np.random.randint(0, 1600, 160)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_e_e = (5 * ms - 0 * ms) * np.random.random(160) + 0 * ms
+            self.wt_e_e = (1./5)*np.random.rand(160)  # unformly distributed random numbers between 0 and 1
+            k = 0
             for i in pos_e_e:
-                self.wt_e_e[i][j] = 200. * (1. / 5) * np.random.random()  # weights have to be between 0 and 0.2
-            self.wt_e_e[j][j] = 0.0   #prevent from connecting a neuron to itself in case it has randomly been assigned
-        nest.Connect(self.nodes_e, self.nodes_e, conn_spec = {'rule': 'all_to_all','autapses': False }, syn_spec = {'model': 'stdp_synapse', 'weight': self.wt_e_e , 'delay': 1.0, 'tau_plus': 15, 'Wmax':0.2})
+                if (i == j):  # to avoid the recurrent connections. There will be no error but one weight will get waste
+                    i = i +1
+                S_e_e.w[i][j] = self.wt_e_e[k]  # weights have to be between 0 and 1
+                S_e_e.delay[i][j] = self.delay_e_e[k]
+                k = k + 1
 
 
         # connections from excitatory population to inhibitory population.
-
-        for j in range(0, 1600):
-            pos_e_i = np.random.randint(0, 400, 40)
+        S_e_i = Synapses(self.nodes_e, self.nodes_i, model=self.eqs_stdp_ee, on_pre=self.eqs_STDP_pre_ee,  on_post = self.eqs_STDP_post_ee)
+        S_e_i.connect()  # all to all connections
+        for j in range(0, 400):
+            pos_e_i = np.random.randint(0, 1600, 160)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_e_i = (2 * ms - 0 * ms) * np.random.random(160) + 0 * ms
+            self.wt_e_i = (1./5)*np.random.rand(160)  # unformly distributed random numbers between 0 and 1
+            k = 0
             for i in pos_e_i:
-                self.wt_e_i[i][j] = 200. * (1. / 5) * np.random.random()  # weights have to be between 0 and 0.2
-        nest.Connect(self.nodes_e, self.nodes_i, conn_spec = {'rule': 'all_to_all'}, syn_spec = {'model': 'stdp_synapse', 'weight': self.wt_e_i , 'delay': 1.0, 'tau_plus': 15, 'Wmax':0.2})
+                S_e_i.w[i][j] = self.wt_e_i[k]  # weights have to be between 0 and 1
+                S_e_i.delay[i][j] = self.delay_e_i[k]
+                k = k + 1
 
         # connections from inhibitory population to excitatory population.
-
-        for j in range(0, 400):
-            pos_i_e =  np.random.randint(0, 1600, 160)
+        S_i_e = Synapses(self.nodes_e, self.nodes_i, model=self.eqs_stdp_ie, on_pre=self.eqs_STDP_pre_ie,  on_post = self.eqs_STDP_post_ie)
+        S_i_e.connect()  # all to all connections
+        for j in range(0, 1600):
+            pos_i_e = np.random.randint(0, 400, 40)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_i_e = (1 * ms - 0 * ms) * np.random.random(40) + 0 * ms
+            self.wt_i_e = np.random.rand(40)  # unformly distributed random numbers between 0 and 1
+            k = 0
             for i in pos_i_e:
-                self.wt_i_e[i][j] = 200. * np.random.random()  # weights have to be between 0 and 1
-        nest.Connect(self.nodes_i, self.nodes_e, conn_spec = {'rule': 'all_to_all'}, syn_spec = {'model': 'stdp_synapse', 'weight': self.wt_i_e , 'delay': 1.0, 'tau_plus': 15, 'Wmax':0.2})
+                S_i_e.w[i][j] = self.wt_i_e[k]  # weights have to be between 0 and 1
+                S_i_e.delay[i][j] = self.delay_i_e[k]
 
-        # connections from inhibitory population to inhibitory population.
-
+         # connections from inhibitory population to inhibitory population.
+        S_i_i = Synapses(self.nodes_i, self.nodes_i, model=self.eqs_stdp_ie, on_pre=self.eqs_STDP_pre_ie,  on_post = self.eqs_STDP_post_ie)
+        S_i_i.connect()  # all to all connections
         for j in range(0, 400):
-            pos_i_i =  np.random.randint(0, 400, 40)
+            pos_i_i = np.random.randint(0, 400, 40)  # assigns random 160 positions between [0 and 1599). Acts as a column of connection matrix
+            self.delay_i_i = (5 * ms - 0 * ms) * np.random.random(40) + 0 * ms
+            self.wt_i_i =  (1 / 2.5) * np.random.rand(40)  # unformly distributed random numbers between 0 and 1
+            k = 0
             for i in pos_i_i:
-                self.wt_i_i[i][j] = 200. * (1 / 2.5) * np.random.random()  # weights have to be between 0 and 0.4
-            self.wt_i_i[j][j] = 0.0  # prevent from connecting a neuron to itself in case it has randomly been assigned
-        nest.Connect(self.nodes_i, self.nodes_i, conn_spec = {'rule': 'all_to_all','autapses': False }, syn_spec = {'model': 'stdp_synapse', 'weight': self.wt_i_i , 'delay': 1.0, 'tau_plus': 15, 'Wmax':0.4})
+                if (i == j):
+                    i = i +1
+                S_i_e.w[i][j] = self.wt_i_i[k]  # weights have to be between 0 and 1
+                S_i_e.delay[i][j] = self.delay_i_i[k]
 
         # to record the spikes from the excitaory neurosn of computation population.
-        self.spikedetector = nest.Create("spike_detector", 1600, params={"withgid": True, "withtime": True})
-        nest.Connect(self.nodes_e, self.spikedetector, 'one_to_one')
-
+        self.spikedetector = SpikeMonitor(self.nodes_e)
 
     def record_spikes(self):
         for i in range(1600):
-            self.spikes[i] = len(nest.GetStatus(self.spikedetector, keys="events")[i]['senders'])
+            self.spikes[i] = len(self.spikedetector.all_values()['t'][i])
         self.new_spikes = self.spikes
         self.spikes = self.spikes - self.old_spikes
         print ('The total number of new spikes of the population are', np.sum(self.spikes))
@@ -305,24 +326,26 @@ class stdp_class:
 
 
     def simulate(self):
-        if nest.NumProcesses() > 1:
-            sys.exit("For simplicity, this example only works for a single process.")
-
         print("Starting simulation")
 
         #sim_steps = numpy.arange(0, self.t_sim, self.record_interval)
-        sim_steps = numpy.arange(0, self.t_sim, self.input_change_interval)  # for training phase
-
+        sim_steps = numpy.arange(0, self.t_sim, self.record_interval)  # for training phase
+        j = 0
         for i, step in enumerate(sim_steps):
-            nest.Simulate(self.input_change_interval)  # simulates the network for the time in the arguments. The argument here is for the training phase
-            self.bg_rate = np.random.rand()   # this is for training examples
+            run(self.input_change_interval)  # simulates the network for the time in the arguments. The argument here is for the training phase
             self.record_spikes()
             self.noise.append(self.noise_estimation())
             if i % 20 == 0:  # happens every 20*1000 ms = 20s
                 print("Progress: " + str(i / 2) + "%")
-            stdp_case.plot_data1()
-            stdp_case.plot_data2()
-            stdp_case.plot_data3()
+
+            if (j == 9):         # happens after 10 * 25 ms = 250  ms        Input is changed now
+                self.bg_rate = 20. * wrap_gaussian(self.xdata, self.mu_input, np.random.random())
+                stdp_case.plot_data1()
+                stdp_case.plot_data2()
+                stdp_case.plot_data3()
+                j = 0
+
+            j = j +1
         print("Simulation finished successfully")
 
 
@@ -360,8 +383,8 @@ if __name__ == '__main__':
     stdp_case.connections()
     # Start simulation
     stdp_case.simulate()
-    stdp_case.plot_data1()
-    stdp_case.plot_data2()
-    stdp_case.plot_data3()
+    #stdp_case.plot_data1()
+    #stdp_case.plot_data2()
+    #stdp_case.plot_data3()
 
     # creating a population of inhibitory and excitatory neurons
